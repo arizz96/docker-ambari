015002b6d71ef08516e056f116382fb28e47f265
arizz96 <arizz96@gmail.com>
2018-05-16 16:02:40 +0200

    First commit

---
+++ ambari-agent/entrypoint.sh
@@ -0,0 +1,6 @@
+echo "[server]" >> /etc/ambari-agent/conf/ambari-agent.ini
+echo "hostname=$AMBARI_SERVER_HOSTNAME" >> /etc/ambari-agent/conf/ambari-agent.ini
+echo "url_port=$URL_PORT" >> /etc/ambari-agent/conf/ambari-agent.ini
+echo "secured_url_port=$SECURED_URL_PORT" >> /etc/ambari-agent/conf/ambari-agent.ini
+
+ambari-agent start
