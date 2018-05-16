7ffdad220a58e3590986a1a75483042e93ff9bbe
arizz96 <arizz96@gmail.com>
2018-05-16 19:09:32 +0200

    Added bash file description to entrypoint

--- ambari-agent/entrypoint.sh
+++ ambari-agent/entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "[server]" >> /etc/ambari-agent/conf/ambari-agent.ini
 echo "hostname=$AMBARI_SERVER_HOSTNAME" >> /etc/ambari-agent/conf/ambari-agent.ini
 echo "url_port=$URL_PORT" >> /etc/ambari-agent/conf/ambari-agent.ini
--- ambari-server/entrypoint.sh
+++ ambari-server/entrypoint.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 ambari-server start
