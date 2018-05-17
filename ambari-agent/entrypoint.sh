c164c840854cdaba112a1d065e50632a977a6229
arizz96 <arizz96@gmail.com>
2018-05-17 09:58:34 +0200

    Added hugepages fix

--- ambari-agent/entrypoint.sh
+++ ambari-agent/entrypoint.sh
@@ -5,4 +5,7 @@ echo -e "\nhostname=$AMBARI_SERVER_HOSTNAME" >> /etc/ambari-agent/conf/ambari-ag
 echo -e "\nurl_port=$URL_PORT" >> /etc/ambari-agent/conf/ambari-agent.ini
 echo -e "\nsecured_url_port=$SECURED_URL_PORT" >> /etc/ambari-agent/conf/ambari-agent.ini
 
+# fix warnings
+hugepages --thp-never
+
 ambari-agent start && tail -f /var/log/ambari-agent/ambari-agent.log
