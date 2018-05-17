d16ecc4fdf6569ac8ba426bf9091512464a798c9
arizz96 <arizz96@gmail.com>
2018-05-17 09:24:55 +0200

    Add newlines in Agent conf initialization

--- ambari-agent/entrypoint.sh
+++ ambari-agent/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "[server]" >> /etc/ambari-agent/conf/ambari-agent.ini
-echo "hostname=$AMBARI_SERVER_HOSTNAME" >> /etc/ambari-agent/conf/ambari-agent.ini
-echo "url_port=$URL_PORT" >> /etc/ambari-agent/conf/ambari-agent.ini
-echo "secured_url_port=$SECURED_URL_PORT" >> /etc/ambari-agent/conf/ambari-agent.ini
+echo -e "\n[server]" >> /etc/ambari-agent/conf/ambari-agent.ini
+echo -e "\nhostname=$AMBARI_SERVER_HOSTNAME" >> /etc/ambari-agent/conf/ambari-agent.ini
+echo -e "\nurl_port=$URL_PORT" >> /etc/ambari-agent/conf/ambari-agent.ini
+echo -e "\nsecured_url_port=$SECURED_URL_PORT" >> /etc/ambari-agent/conf/ambari-agent.ini
 
 ambari-agent start && tail -f /var/log/ambari-agent/ambari-agent.log
